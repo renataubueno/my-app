ff0a66f02be9bcb074b98d7cc3a72e302b0e570f
renataubueno <renataubueno@gmail.com>
2019-03-19 17:57:17 -0300

    Changes made on app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import mtg from './mtg.jpg';
+import doge from './doge.jpeg';
 import './App.css';
 
 class App extends Component {
@@ -7,18 +8,17 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        	<img id="image" src={mtg} alt="Approach of the Second Sun" position="relative" width="1200" height="430"/>
+        	<p>Em busca de cartinhas bem baratas?</p>
+        	<form id="myformmbb" action="https://www.magicbembarato.com.br/">
+    			<input type="submit" value="Magic Bem Barato"/>
+			</form>
+			<form id="myformkinoene" action="https://www.kinoenecards.com.br/">
+    			<input type="submit" value="Kinoene"/>
+			</form>
+			<form id="myformcop" action="https://www.cardsofparadise.com.br/">
+    			<input type="submit" value="Cards of Paradise"/>
+			</form>
         </header>
       </div>
     );
