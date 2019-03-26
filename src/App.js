da58d0ddb8e44530c1c7be75709145f38acc718a
renataubueno <renataubueno@gmail.com>
2019-03-26 17:18:33 -0300

    Played around with Material-UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,198 @@
-import React, { Component } from 'react';
-import mtg from './mtg.jpg';
-import doge from './doge.jpeg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
+import Select from '@material-ui/core/Select';
+import Input from '@material-ui/core/Input';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FilledInput from '@material-ui/core/FilledInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
 
-class App extends Component {
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    marginRight: 50,
+  	padding: '0 8px',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+});
+
+class PersistentDrawerLeft extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
+    const { classes, theme } = this.props;
+    const { open } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-        	<img id="image" src={mtg} alt="Approach of the Second Sun" position="relative" width="1200" height="430"/>
-        	<p>Em busca de cartinhas bem baratas?</p>
-        	<form id="myformmbb" action="https://www.magicbembarato.com.br/">
-    			<input type="submit" value="Magic Bem Barato"/>
-			</form>
-			<form id="myformkinoene" action="https://www.kinoenecards.com.br/">
-    			<input type="submit" value="Kinoene"/>
-			</form>
-			<form id="myformcop" action="https://www.cardsofparadise.com.br/">
-    			<input type="submit" value="Cards of Paradise"/>
-			</form>
-        </header>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={classNames(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar disableGutters={!open}>
+            <IconButton
+              color="secondary"
+              aria-label="Open drawer"
+              onClick={this.handleDrawerOpen}
+              className={classNames(classes.menuButton, open && classes.hide)}
+            >
+            </IconButton>
+            <Typography variant="h6" color="inherit" noWrap>
+              Queue Network Simulator
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="left"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton color="secondary" onClick={this.handleDrawerClose}>
+            </IconButton>
+          </div>
+          <Divider />
+          <div>
+          Objetos
+          </div>
+          <Divider />
+          <InputLabel htmlFor="stop-helper">Condição de Parada</InputLabel>
+          <Select
+            value={this.state.stop}
+            onChange={this.handleChange}
+          >
+            <MenuItem value="">
+              <em>Condição de Parada</em>
+            </MenuItem>
+            <MenuItem value={1}>Tempo</MenuItem> //1 para tempo
+            <MenuItem value={2}>Número de Chegadas</MenuItem> //2 para número de chegadas
+          </Select>
+          <InputLabel htmlFor="type-helper">Tipo de Distriuição</InputLabel>
+          <Select
+            value={this.state.type}
+            onChange={this.handleChange}
+          >
+            <MenuItem value="">
+              <em>Tipo de Distriuição</em>
+            </MenuItem>
+            <MenuItem value={1}>Uniforme</MenuItem> //1 para uniforme
+            <MenuItem value={2}>Exponencial</MenuItem> //2 para exponencial
+            <MenuItem value={3}>Geométrica</MenuItem> //3 para geométrica
+            <MenuItem value={4}>Binomial</MenuItem> //4 para binomial
+          </Select>
+          <Button variant="contained" className={classes.button}>
+        	Iniciar Simulação
+      	  </Button>
+          <Divider />
+          <Button variant="contained" className={classes.button}>
+        	Exportar XML
+      	  </Button>
+      	  <Button variant="contained" className={classes.button}>
+        	Importar XML
+      	  </Button>
+        </Drawer>
+        <main
+          className={classNames(classes.content, {
+            [classes.contentShift]: open,
+          })}
+        >
+          <div className={classes.drawerHeader} />
+          EDITOR
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+PersistentDrawerLeft.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(PersistentDrawerLeft);
