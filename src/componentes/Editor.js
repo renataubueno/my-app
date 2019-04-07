20ea956b0484e846ca6286a8293384a07b3c837a
renataubueno <renataubueno@gmail.com>
2019-04-07 16:50:18 -0300

    Added the Editor box (just to have an idea of where the editor will be)

--- src/componentes/Editor.js
+++ src/componentes/Editor.js
@@ -2,14 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 
 const styles = theme => ({
   drawerHeader: {
+    width: '300px',
+    height: '100px',
     display: 'flex',
     alignItems: 'center',
-    padding: '0 8px',
+    padding: '100px 50px 75px 100px',
     ...theme.mixins.toolbar,
     justifyContent: 'flex-end',
+  },root: {
+    width: '800px',
+    height: '400px',
+    alignItems: 'center',
+    marginLeft: '245px',
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 4,
+    paddingBottom: theme.spacing.unit * 5,
   },
 });
 
@@ -20,9 +31,13 @@ class Editor extends React.Component{
     return(
       <main>
         <div className={classes.drawerHeader} />
+        <Paper className={classes.root} elevation={1}>
         <Typography variant="h5" component="h3">
           EDITOR
         </Typography>
+        <Typography component="p">
+        </Typography>
+      </Paper>
       </main>
     );
   }
