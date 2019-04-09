23bca08c6bc847600cf0f2765dbb207bbb037f45
renataubueno <renataubueno@gmail.com>
2019-04-09 20:53:52 -0300

    added the pubsub when clicking on the Iniciar Simulação button

--- src/componentes/Relatorio.js
+++ src/componentes/Relatorio.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
+import Pubsub from 'pubsub-js';
 
 export default class Relatorio extends Component{
   constructor(props){
@@ -9,38 +10,50 @@ export default class Relatorio extends Component{
       feed: [
         {idFila: 1, numChegadas: 10, numAtendimentos: 120, tempoOcupada: 122, taxaChegada: 14, vazao: 52, utilizacao: 42,   tempoMedioServico: 13},
         {idFila: 2, numChegadas: 15, numAtendimentos: 66, tempoOcupada: 19, taxaChegada: 1, vazao: 13, utilizacao: 24,   tempoMedioServico: 55}
-      ]
+      ],
+      filas: []
     }
   }
+
+  componentWillMount(){
+      Pubsub.subscribe('retorno-fila', (topico, dadosDaFila) => {
+        console.log('Chegou : ', dadosDaFila.resposta);
+        this.setState({filas: dadosDaFila.resposta});
+        console.log('Conteúdo da fila: ', this.state.filas[0].id);
+        //this.state.filas -> retorna os dois objetos
+        //this.state.filas[0] -> retorna o primeiro objetos
+        //this.state.filas[0].id -> retorna o id do primeiro objeto
+     });
+ }
 
   render(){
     return(
       <div color="primary">
         {
-          this.state.feed.map(item => (
+          this.state.filas.map(item => (
             <div>
-              <Typography key={item.idFila} align="center" variant="subtitle1" color="primary" noWrap >
-                Fila {item.idFila}
+              <Typography key={item.id} align="center" variant="subtitle1" color="primary" noWrap >
+                Fila {item.id}
               </Typography>
-              <Typography key={item.numChegadas + item.idFila} align="center" variant="body1" color="primary" noWrap>
+              <Typography key={item.numChegadas + item.id} align="center" variant="body1" color="primary" noWrap>
                 Número de Chegadas: {item.numChegadas}
               </Typography>
-              <Typography key={item.numAtendimentos + item.idFila} align="center" variant="body1" color="primary" noWrap>
+              <Typography key={item.numAtendimentos + item.id} align="center" variant="body1" color="primary" noWrap>
                 Número de Atendimentos: {item.numAtendimentos}
               </Typography>
-              <Typography key={item.tempoOcupada + item.idFila} align="center" variant="body1" color="primary" noWrap>
+              <Typography key={item.tempoOcupada + item.id} align="center" variant="body1" color="primary" noWrap>
                 Tempo Ocupada: {item.tempoOcupada}
               </Typography>
-              <Typography key={item.taxaChegada + item.idFila} align="center" variant="body1" color="primary" noWrap>
+              <Typography key={item.taxaChegada + item.id} align="center" variant="body1" color="primary" noWrap>
                 Taxa de Chegada: {item.taxaChegada}
               </Typography>
-              <Typography key={item.vazao + item.idFila}  align="center" variant="body1" color="primary" noWrap>
+              <Typography key={item.vazao + item.id}  align="center" variant="body1" color="primary" noWrap>
                 Vazão: {item.vazao}
               </Typography>
-              <Typography key={item.utilizacao + item.idFila} align="center" variant="body1" color="primary" noWrap>
+              <Typography key={item.utilizacao + item.id} align="center" variant="body1" color="primary" noWrap>
                 Utilização: {item.utilizacao}
               </Typography>
-              <Typography key={item.tempoMedioServico + item.idFila} align="center" variant="body1" color="primary" noWrap>
+              <Typography key={item.tempoMedioServico + item.id} align="center" variant="body1" color="primary" noWrap>
                 Tempo Médio de Serviço: {item.tempoMedioServico}
               </Typography>
               <Divider />
--- src/componentes/Simulacao.js
+++ src/componentes/Simulacao.js
@@ -1,11 +1,35 @@
 import React, {Component} from 'react';
 import Button from '@material-ui/core/Button';
+import Pubsub from 'pubsub-js';
 
 export default class Simulacao extends Component{
+  handleClick = control => event =>{
+   const requestInfo = {
+     method: 'GET'
+   }
+
+   fetch('http://localhost:3000/fila', requestInfo)
+     .then(response => {
+      if(response.ok){
+        return response.json();
+      }else{
+        throw new Error('Não foi possível comentar');
+      }
+    })
+    .then(fila => {
+      console.log('Resposta: ', fila);
+
+      Pubsub.publish('retorno-fila', {
+        id: fila[0].id,
+        resposta: fila
+      });
+    });
+ }
+
   render(){
     return(
       <div>
-      <Button variant="contained" /*className={classes.button}*/>
+      <Button variant="contained" onClick={ this.handleClick('control') }>
         Iniciar Simulação
       </Button>
       </div>
