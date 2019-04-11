404c05ff685c04288beb78cca192215bfe279691
renataubueno <renataubueno@gmail.com>
2019-04-11 17:32:46 -0300

    added the images of the objects + onClick events when clicking on the images

--- src/componentes/DrawerLeftSidebar.js
+++ src/componentes/DrawerLeftSidebar.js
@@ -8,12 +8,32 @@ import Simulacao from './Simulacao.js';
 import Typography from '@material-ui/core/Typography';
 import MenuDistribuicao from './MenuDistribuicao.js';
 import MenuParada from './MenuParada.js';
+import circleImage from '../images/circle.png';
+import squareImage from '../images/square.png';
+import triangleImage from '../images/triangle.png';
+import arrowImage from '../images/arrow.png';
 
 export default class DrawerLeftSidebar extends Component{
   constructor(props){
     super(props);
     this.state = {open: false};
   }
+
+  handleClickCircle = control => event => {
+    console.log('Cliquei no Círculo');
+  };
+
+  handleClickSquare = control => event => {
+    console.log('Cliquei no Quadrado');
+  };
+
+  handleClickTriangle = control => event => {
+    console.log('Cliquei no Triângulo');
+  };
+
+  handleClickArrow = control => event => {
+    console.log('Cliquei na Flecha');
+  };
 
   render(){return(
       <div className={'left-drawer-header'}>
@@ -23,7 +43,10 @@ export default class DrawerLeftSidebar extends Component{
         <Divider />
         <div>
         <Typography align="center" variant="h6" color="primary" noWrap>
-          OBJETOS
+          <img src={circleImage} alt="CIRCLE" height="60" width="60" onClick={ this.handleClickCircle('control') }/> &nbsp; &nbsp; &nbsp; &nbsp;
+          <img src={squareImage} alt="SQUARE" height="45" width="45" onClick={ this.handleClickSquare('control') } /> <br />
+          <img src={triangleImage} alt="TRIANGLE" height="45" width="45" onClick={ this.handleClickTriangle('control') } /> &nbsp; &nbsp; &nbsp; &nbsp;
+          <img src={arrowImage} alt="ARROW" height="45" width="45" onClick={ this.handleClickArrow('control') } />
         </Typography>
         </div>
         <Divider />
