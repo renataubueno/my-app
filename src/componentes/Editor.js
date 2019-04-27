e1c0ea4344f211f6058d066206a346329aaffbec
renataubueno <renataubueno@gmail.com>
2019-04-27 08:11:18 -0300

    changes made to the Editor.js and FilaEditor.js

--- src/componentes/Editor.js
+++ src/componentes/Editor.js
@@ -4,6 +4,12 @@ import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Pubsub from 'pubsub-js';
 import Draggable from 'react-draggable';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
 
 import FilaEditor from './FilaEditor.js';
 import ConectorEditor from './ConectorEditor.js';
@@ -32,12 +38,23 @@ class Editor extends React.Component{
       filaFilas: [],
       filaConector: [],
       filaSaida: [],
-      filaEntrada: []
+      filaEntrada: [],
+      open: false,
+      id: 0,
+      capacidade: 0,
+      servidores: 0,
+      minChegada: 0,
+      maxChegada: 0,
+      minServico: 0,
+      maxServico: 0,
     }
+
+    this._handleDoubleClickOpen = this._handleDoubleClickOpen.bind(this);
+    this._handleDoubleClickClose = this._handleDoubleClickClose.bind(this);
   }
 
   componentWillMount(){
-      Pubsub.subscribe('retorno-fila', (topico, dadosDaFila) => {
+    Pubsub.subscribe('retorno-fila', (topico, dadosDaFila) => {
         console.log('Chegou : ', dadosDaFila.resposta);
         var itemsFila = [ ].concat(this.state.filaFilas);
         itemsFila.push(dadosDaFila.resposta);
@@ -45,7 +62,7 @@ class Editor extends React.Component{
         console.log('Conteúdo da fila de filas: ', this.state.filaFilas);
      });
 
-     Pubsub.subscribe('retorno-conector', (topico, dadosDoConector) => {
+    Pubsub.subscribe('retorno-conector', (topico, dadosDoConector) => {
        console.log('Chegou : ', dadosDoConector.resposta);
        var itemsConector = [ ].concat(this.state.filaConector);
        itemsConector.push(dadosDoConector.resposta);
@@ -76,6 +93,11 @@ class Editor extends React.Component{
       this.setState({filaSaida: []});
       this.setState({filaEntrada: []});
     });
+
+    Pubsub.subscribe('double-click', (topico, dados) => {
+      console.log('Chegou double-click: ', dados);
+      this.setState({open: true, id: dados.id});
+   });
  }
 
  trataFilas = () => {
@@ -110,6 +132,87 @@ class Editor extends React.Component{
    );
  };
 
+
+   _handleDoubleClickOpen(event): void {
+     this.setState({open: true});
+   }
+
+   _handleDoubleClickClose(event): void {
+     this.setState({open: false});
+   }
+
+   handleChange = parametro => event => {
+     this.setState({ [parametro]: parseInt(event.target.value) });
+   };
+
+   handleDelete = event => {
+     Pubsub.publish('deletar-fila-unif', {
+       id: 'this.props.objeto.id'
+     });
+   };
+
+   handleDialog = () => {
+     return (
+       <DialogContent>
+       <TextField
+          id="standard-name"
+          label="Servidores:"
+          className={'chegada-text-field'}
+          value={this.state.servidores}
+          onChange={this.handleChange('servidores')}
+          margin="normal"
+        />
+        <TextField
+           id="standard-name"
+           label="Capacidade:"
+           className={'capacidade-text-field'}
+           value={this.state.capacidade}
+           onChange={this.handleChange('capacidade')}
+           margin="normal"
+         />
+        <TextField
+           id="standard-name"
+           label="id da Fila"
+           className={'idFila-text-field'}
+           value={this.state.id}
+           margin="normal"
+         />
+         <TextField
+            id="standard-name"
+            label="Número Mínimo de Chegadas"
+            className={'condicao-min-chegada-text-field'}
+            value={this.state.minChegada}
+            onChange={this.handleChange('minChegada')}
+            margin="normal"
+          />
+          <TextField
+             id="standard-name"
+             label="Número Máximo de Chegadas"
+             className={'condicao-max-chegada-text-field'}
+             value={this.state.maxChegada}
+             onChange={this.handleChange('maxChegada')}
+             margin="normal"
+           />
+           <TextField
+              id="standard-name"
+              label="Número Mínimo de Serviço"
+              className={'condicao-min-servico-text-field'}
+              value={this.state.minServico}
+              onChange={this.handleChange('minServico')}
+              margin="normal"
+            />
+            <TextField
+               id="standard-name"
+               label="Número Máximo de Serviço"
+               className={'condicao-max-servico-text-field'}
+               value={this.state.maxServico}
+               onChange={this.handleChange('maxServico')}
+               margin="normal"
+             />
+       </DialogContent>
+     );
+   };
+
   render(){
     const { classes } = this.props;
 
@@ -121,6 +224,20 @@ class Editor extends React.Component{
         { this.trataConector() }
         { this.trataSaida() }
         { this.trataEntrada() }
+        <Dialog open={this.state.open} onClose={this._handleDoubleClickClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+          <DialogTitle id="alert-dialog-title">
+            {"Parâmetros da Fila"}
+          </DialogTitle>
+          {this.handleDialog()}
+          <DialogActions>
+            <Button onClick={this._handleDoubleClickClose} color="primary">
+              Ok
+            </Button>
+            <Button onClick={this.handleDelete} color="primary">
+              Deletar Objeto
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Paper>
       </main>
     );
--- src/componentes/FilaEditor.js
+++ src/componentes/FilaEditor.js
@@ -15,120 +15,22 @@ export default class FilaEditor extends Objeto {
   constructor(props){
     super(props);
 
-    this.state = {
-      capacidade: 0,
-      servidores: 0,
-      minChegada: 0,
-      maxChegada: 0,
-      minServico: 0,
-      maxServico: 0,
-    }
-
     this._handleDoubleClickOpen = this._handleDoubleClickOpen.bind(this);
-    this._handleDoubleClickClose = this._handleDoubleClickClose.bind(this);
   }
 
   _handleDoubleClickOpen(event): void {
-    this.setState({open: true});
-  }
-
-  _handleDoubleClickClose(event): void {
-    this.setState({open: false});
-  }
-
-  handleChange = parametro => event => {
-    this.setState({ [parametro]: parseInt(event.target.value) });
-  };
-
-  handleDelete = event => {
-    Pubsub.publish('deletar-fila-unif', {
-      id: this.props.objeto.id
+    Pubsub.publish('double-click', {
+      tipoObjeto: 'Fila',
+      id: this.dadosDoObjeto.key
     });
-  };
-
-  handleDialog = () => {
-    return (
-      <DialogContent>
-      <TextField
-         id="standard-name"
-         label="Servidores:"
-         className={'chegada-text-field'}
-         value={this.state.servidores}
-         onChange={this.handleChange('servidores')}
-         margin="normal"
-       />
-       <TextField
-          id="standard-name"
-          label="Capacidade:"
-          className={'capacidade-text-field'}
-          value={this.state.capacidade}
-          onChange={this.handleChange('capacidade')}
-          margin="normal"
-        />
-       <TextField
-          id="standard-name"
-          label="id da Fila"
-          className={'idFila-text-field'}
-          value={this.props.objeto.id}
-          margin="normal"
-        />
-        <TextField
-           id="standard-name"
-           label="Número Mínimo de Chegadas"
-           className={'condicao-min-chegada-text-field'}
-           value={this.state.minChegada}
-           onChange={this.handleChange('minChegada')}
-           margin="normal"
-         />
-         <TextField
-            id="standard-name"
-            label="Número Máximo de Chegadas"
-            className={'condicao-max-chegada-text-field'}
-            value={this.state.maxChegada}
-            onChange={this.handleChange('maxChegada')}
-            margin="normal"
-          />
-          <TextField
-             id="standard-name"
-             label="Número Mínimo de Serviço"
-             className={'condicao-min-servico-text-field'}
-             value={this.state.minServico}
-             onChange={this.handleChange('minServico')}
-             margin="normal"
-           />
-           <TextField
-              id="standard-name"
-              label="Número Máximo de Serviço"
-              className={'condicao-max-servico-text-field'}
-              value={this.state.maxServico}
-              onChange={this.handleChange('maxServico')}
-              margin="normal"
-            />
-      </DialogContent>
-    );
-  };
+  }
 
   render(){
     return(
-    <div>
       <Draggable {...this.settings}>
         <img src={FilaImage} alt="Fila" {...this.dadosDoObjeto} onDoubleClick={this._handleDoubleClickOpen}/>
       </Draggable>
-      <Dialog open={this.state.open} onClose={this._handleDoubleClickClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
-        <DialogTitle id="alert-dialog-title">
-          {"Parâmetros da Fila"}
-        </DialogTitle>
-        {this.handleDialog()}
-        <DialogActions>
-          <Button onClick={this._handleDoubleClickClose} color="primary">
-            Ok
-          </Button>
-          <Button onClick={this.handleDelete} color="primary">
-            Deletar Objeto
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+
     );
   }
 }
