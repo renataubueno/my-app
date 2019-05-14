bda743dd547aae4a7064e31c02a077e9f059e334
renataubueno <renataubueno@gmail.com>
2019-05-14 19:25:03 -0300

    GET and POST are working with test data, need to make it work with real data

--- express/routes/simulacao.js
+++ express/routes/simulacao.js
@@ -1,21 +1,24 @@
 var express = require('express');
+var service = require('../services/simulacaoService');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send({
-    id: 1,
-    numChegadas: 2,
-    numAtendimentos: 50,
-    tempoOcupada: 10,
-    taxaChegada: 3,
-    vazao: 2,
-    utilizacao: 1,
-    tempoMedioServico: 1
-  });
+  res.status(200);
+  res.send(service());
 });
 
 router.post('/', function(req, res, next) {
-  res.send('ESTOU NO POST PLMDDS');
+  let body = req.body; //o body está com o conteúdo do POST do Simulacao.js
+  console.log('BODY: ', body);
+  //validar os dados do body
+  let retorno = service(body); //mando as informações recebidas para o serviço.
+  //Lá devo fazer o papel de tratar as informações do body e armazenar elas
+  console.log('RETORNO: ', retorno);
 });
+
+//Passo a passo do POST:
+//Pegar as informações de fila e mandar através do body para o service
+//No service, devo tratar as informações
+//Tenho que descobrir como salvar as informações num arquivo JSON
 
 module.exports = router;
---
+++ express/services/simulacaoService.js
@@ -0,0 +1,14 @@
+var simula = function (dados) {
+  return {
+      id: 1,
+      numChegadas: "A",
+      numAtendimentos: "B",
+      tempoOcupada: "C",
+      taxaChegada: "A/T",
+      vazao: "C/T",
+      Utilizacao: "B/T",
+      tempoMedioServico: "B/C"
+  };
+}
+
+module.exports = simula;
