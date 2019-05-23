613c2a1fee45d7553089d21746dc2b2b44eb0248
renataubueno <renataubueno@gmail.com>
2019-05-23 14:46:58 -0300

    graph is showing the percentage of time the queue stays occupied in each state

--- src/componentes/Relatorio.js
+++ src/componentes/Relatorio.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 import Pubsub from 'pubsub-js';
+import { PieChart } from 'react-charts-d3';
 
 export default class Relatorio extends Component{
   constructor(props){
@@ -13,18 +14,32 @@ export default class Relatorio extends Component{
   }
 
   componentWillMount(){
-     Pubsub.subscribe('post-retorno', (topico, retorno) => {
-       console.log('O QUE TEM NO RETORNO DO RELATÕRIO?' , retorno);
+     Pubsub.subscribe('post-retorno', (topico, data) => {
+       console.log('O QUE TEM NO RETORNO DO RELATÕRIO?' , data);
        this.setState({retorno: []});
-       this.setState({retornoProb: []});
        var itemsRetorno = [ ].concat(this.state.retorno);
-       itemsRetorno.push(retorno.retorno);
-       var itemsRetornoProb = [ ].concat(this.state.retornoProb);
-       itemsRetornoProb.push(retorno.retorno.probabilidadesEstadosFila);
+       itemsRetorno.push(data.retorno);
        this.setState({retorno: itemsRetorno});
-       this.setState({retornoProb: itemsRetornoProb});
-       console.log(this.state.retornoProb);
+       this.setState({retornoProb: data.retorno.probabilidadesEstadosFila});
     });
+ }
+
+ probGraph = () => {
+  let data = [];
+
+  console.log('PQ ELE N FUNCIONA DE PRIMEIRA? ', this.state.retornoProb[0]);
+  console.log('PQ ELE N FUNCIONA DE PRIMEIRA? ', this.state.retornoProb[2]);
+
+  for(let i = 0; i < this.state.retornoProb.length; i++){
+    let estadoAtual = i.toString();
+    let obj = {label: estadoAtual, value: this.state.retornoProb[i]};
+    console.log('O QUE TEM NO OBJ? ', obj);
+    data.push(obj);
+  }
+
+  console.log('O QUE TEM NO DATA? ', data);
+
+  return <div> <PieChart data={data} /> /> </div>
  }
 
   render(){
@@ -43,31 +58,32 @@ export default class Relatorio extends Component{
                 Número de Atendimentos: {item.numAtendimentos}
               </Typography>
               <Typography key={item.tempoOcupada + item.id} align="center" variant="body1" color="primary" noWrap>
-                Tempo Ocupada: {item.tempoOcupada}
+                Tempo Ocupada: {item.tempoOcupada.toFixed(2)}
               </Typography>
               <Typography key={item.taxaChegada + item.id} align="center" variant="body1" color="primary" noWrap>
-                Taxa de Chegada: {item.taxaChegada}
+                Taxa de Chegada: {item.taxaChegada.toFixed(2)}
               </Typography>
               <Typography key={item.vazao + item.id}  align="center" variant="body1" color="primary" noWrap>
-                Vazão: {item.vazao}
+                Vazão: {item.vazao.toFixed(2)}
               </Typography>
               <Typography key={item.utilizacao + item.id} align="center" variant="body1" color="primary" noWrap>
-                Utilização: {item.utilizacao}
+                Utilização: {item.utilizacao.toFixed(2)}
               </Typography>
               <Typography key={item.tempoMedioServico + item.id} align="center" variant="body1" color="primary" noWrap>
-                Tempo Médio de Serviço: {item.tempoMedioServico}
+                Tempo Médio de Serviço: {item.tempoMedioServico.toFixed(2)}
               </Typography>
               <Divider />
-                <Typography key={item.id + 10} align="center" variant="subtitle1" color="primary" noWrap >
-                  Probabilidade da Fila {item.id}
-                </Typography>
+              <Typography key={item.id + 10} align="center" variant="subtitle1" color="primary" noWrap >
+                Probabilidades da Fila {item.id}
+              </Typography>
+              { this.probGraph() }
             </div>
           ))
         }
         {
           this.state.retornoProb.map(val => (
             <Typography key={val}  align="center" variant="body1" color="primary" noWrap>
-              {val}
+              {val.toFixed(2)} %
             </Typography>
           ))
         }
