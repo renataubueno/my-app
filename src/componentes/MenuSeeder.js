784fd5862798c088095c8bc4c9cc52c314c7901f
Brise <gabriel_ns@yahoo.com.br>
2019-06-14 14:00:04 -0300

    Random seed now updates Simulacao on mount

--- src/componentes/MenuSeeder.js
+++ src/componentes/MenuSeeder.js
@@ -1,58 +1,68 @@
-import React, {Component} from 'react';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import Pubsub from 'pubsub-js';
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Pubsub from "pubsub-js";
 
-export default class MenuSeeder extends Component{
+export default class MenuSeeder extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      seeder: this.randomSeed()
+      seeder: 0
     };
   }
 
   randomSeed = () => {
-    return Math.floor((Math.random() * 100) + 1);
-  }
+    let randomSeed = Math.floor(Math.random() * 100 + 1);
+    Pubsub.publish("retorno-seeder", {
+      seeder: randomSeed
+    });
+    this.setState({ seeder: randomSeed });
+  };
 
   handleChange = seeder => event => {
     this.setState({ [seeder]: parseInt(event.target.value, 10) });
   };
 
   handleClick = seeder => event => {
-    console.log('Este é o valor salvo no momento (seeder): ' + this.state.seeder);
+    console.log(
+      "Este é o valor salvo no momento (seeder): " + this.state.seeder
+    );
 
-    Pubsub.publish('retorno-seeder', {
+    Pubsub.publish("retorno-seeder", {
       seeder: this.state.seeder
     });
+  };
+
+  componentDidMount() {
+    this.randomSeed();
   }
 
-  handleFocus = (event) => event.target.select();
+  handleFocus = event => event.target.select();
 
-  render(){
-    return(
+  render() {
+    return (
       <div style={seederStyle}>
         <TextField
-           id="standard-name"
-           label="Seed"
-           className={'seeder-text-field'}
-           style={seederInputStyle}
-           value={this.state.seeder}
-           defaultValue={this.state.seeder}
-           onChange={this.handleChange('seeder')}
-           margin="normal"
-           onFocus={this.handleFocus}
-           onBlur={this.handleClick('seeder')}
-         />
+          id="standard-name"
+          label="Seed"
+          className={"seeder-text-field"}
+          style={seederInputStyle}
+          value={this.state.seeder}
+          defaultValue={this.state.seeder}
+          onChange={this.handleChange("seeder")}
+          margin="normal"
+          onFocus={this.handleFocus}
+          onBlur={this.handleClick("seeder")}
+        />
       </div>
     );
   }
 }
 
 const seederStyle = {
-  padding: '8px 15px 8px 15px',
-}
+  padding: "8px 15px 8px 15px"
+};
 
 const seederInputStyle = {
-  margin: '0'
-}
+  margin: "0"
+};
