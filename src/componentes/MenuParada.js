599dd95b16df9886dad5908dd7dcff12931de705
renataubueno <renataubueno@gmail.com>
2019-06-21 10:34:24 -0300

    Update MenuParada.js

--- src/componentes/MenuParada.js
+++ src/componentes/MenuParada.js
@@ -20,22 +20,23 @@ export default class MenuParada extends Component{
     this.showForm = this.showForm.bind(this);
   }
 
-  opcoesParada(event) {
-    this.setState({value: event.target.value});
+  opcoesParada = (event) => {
+    this.setState({value: event.target.value}, this.showForm);
   }
 
   showForm = () => {
-        if(this.state.value === 'Número de Chegadas'){
-          this.setState( { showChegadas : true } )
-          this.setState( { showTempo : false } )
-        } else if (this.state.value === 'Tempo'){
-          this.setState( { showChegadas : false } )
-          this.setState( { showTempo : true } )
-        }
-        else {
-          this.setState( { showChegadas : false } )
-          this.setState( { showTempo : false } )
-        }
+        if (this.state.value === 'Número de Chegadas') {
+      this.setState( {showChegadas: true})
+      this.setState( {showTempo: false})
+    }
+    else if (this.state.value === 'Tempo') {
+      this.setState( {showChegadas: false} )
+      this.setState( {showTempo: true} )
+    }
+    else {
+      this.setState( {showChegadas : false})
+      this.setState( {showTempo : false})
+    }
   }
 
   render(){
@@ -49,7 +50,6 @@ export default class MenuParada extends Component{
         <MenuItem value={'Tempo'}>Tempo de Simulação</MenuItem>
         <MenuItem value={'Número de Chegadas'}>Número de Chegadas</MenuItem>
       </Select>
-      <Button onClick={ this.showForm}>Ok</Button>
       { this.state.showChegadas && <FormMenuParadaChegadas/>}
       { this.state.showTempo && <FormMenuParadaTempo/>}
       </div>
